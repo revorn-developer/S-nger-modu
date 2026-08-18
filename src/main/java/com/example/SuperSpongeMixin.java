2c08ac799f1b4eda06fdd8331065d317c7d7b4ad
revorn-developer <frecxy71@gmail.com>
2026-08-18 14:42:24 +0300

    Update SuperSpongeMixin.java

--- src/main/java/com/example/SuperSpongeMixin.java
+++ src/main/java/com/example/SuperSpongeMixin.java
@@ -1,11 +1,11 @@
 package com.example.revorn.mixin;
 
-import net.minecraft.block.SpongeBlock;
+import net.minecraft.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(SpongeBlock.class)
+@Mixin(Block.class)
 public class SuperSpongeMixin {
 
     @ModifyConstant(method = "absorbWater", constant = @Constant(intValue = 64))
